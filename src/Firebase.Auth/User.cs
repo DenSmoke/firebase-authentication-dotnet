7c746ed75779658b523078c83c5a2ab6840c6b1c
Денис Перевозчиков <densmoke@live.ru>
2019-08-10 11:55:41 +0300

    Modified User

--- src/Firebase.Auth/User.cs
+++ src/Firebase.Auth/User.cs
@@ -1,8 +1,8 @@
-﻿namespace Firebase.Auth
-{
-    using System.ComponentModel;
-    using System.Text.Json.Serialization;
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
 
+namespace Firebase.Auth
+{
 
     /// <summary>
     /// Basic information about the logged in user.
@@ -14,98 +14,62 @@
         /// </summary>
         [JsonPropertyName("localId")]
         [DefaultValue("")]
-        public string LocalId
-        {
-            get;
-            set;
-        }
+        public string LocalId { get; set; }
 
         /// <summary>
         /// Gets or sets the federated id.
         /// </summary>
         [JsonPropertyName("federatedId")]
         [DefaultValue("")]
-        public string FederatedId
-        {
-            get;
-            set;
-        }
+        public string FederatedId { get; set; }
 
         /// <summary>
         /// Gets or sets the first name.
         /// </summary>
         [JsonPropertyName("firstName")]
         [DefaultValue("")]
-        public string FirstName
-        {
-            get;
-            set;
-        }
+        public string FirstName { get; set; }
 
         /// <summary>
         /// Gets or sets the last name.
         /// </summary>
         [JsonPropertyName("lastName")]
         [DefaultValue("")]
-        public string LastName
-        {
-            get;
-            set;
-        }
+        public string LastName { get; set; }
 
         /// <summary>
         /// Gets or sets the display name.
         /// </summary>
         [JsonPropertyName("displayName")]
         [DefaultValue("")]
-        public string DisplayName
-        {
-            get;
-            set;
-        }
+        public string DisplayName { get; set; }
 
         /// <summary>
         /// Gets or sets the email.
         /// </summary>
         [JsonPropertyName("email")]
         [DefaultValue("")]
-        public string Email
-        {
-            get;
-            set;
-        }
+        public string Email { get; set; }
 
         /// <summary>
         /// Gets or sets the email verfication status.
         /// </summary>
         [JsonPropertyName("emailVerified")]
         [DefaultValue(false)]
-        public bool IsEmailVerified
-        {
-            get;
-            set;
-        }
+        public bool IsEmailVerified { get; set; }
 
         /// <summary>
         /// Gets or sets the photo url.
         /// </summary>
         [JsonPropertyName("photoUrl")]
         [DefaultValue("")]
-        public string PhotoUrl
-        {
-            get;
-            set;
-        }
+        public string PhotoUrl { get; set; }
 
         /// <summary>
         /// Gets or sets the phone number.
         /// </summary>
         [JsonPropertyName("phoneNumber")]
         [DefaultValue("")]
-        public string PhoneNumber
-        {
-            get;
-            set;
-        }
+        public string PhoneNumber { get; set; }
     }
 }
