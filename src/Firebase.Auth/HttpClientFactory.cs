69284bd0038cbec233129e5b4a3012c5e2b6e2f8
Денис Перевозчиков <densmoke@live.ru>
2019-12-31 01:45:05 +0300

    Modified HttpClientFactory

--- src/Firebase.Auth/HttpClientFactory.cs
+++ src/Firebase.Auth/HttpClientFactory.cs
@@ -1,9 +1,26 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Firebase.Auth
 {
     internal class HttpClientFactory : IHttpClientFactory
     {
-        public HttpClient CreateClient(string name) => new HttpClient();
+        private async void DisposeHttpClient(HttpClient client)
+        {
+            await Task.Delay(TimeSpan.FromMinutes(2)).ConfigureAwait(false);
+            try
+            {
+                client.Dispose();
+            }
+            catch { }
+        }
+
+        public HttpClient CreateClient(string name)
+        {
+            var client = new HttpClient();
+            DisposeHttpClient(client);
+            return client;
+        }
     }
 }
