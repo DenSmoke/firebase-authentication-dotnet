89765da37ae1beae0b79d99c7891abed356becec
Max <SijyKijy@users.noreply.github.com>
2020-11-18 09:50:33 +0300

    Adding AddFirebaseHttpClient (#9)

---
+++ src/Firebase.Auth/ServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Firebase.Auth
+{
+    /// <summary>
+    /// Расширение для <see cref="IServiceCollection"/> c настройкой под <see cref="FirebaseAuthProvider"/>
+    /// </summary>
+    public static class ServiceCollectionExtension
+    {
+        /// <summary>
+        /// Добавление <see cref="IHttpClientBuilder"/> к <see cref="IServiceCollection"/> c настройкой:
+        /// <br> Название HttpClient = <see cref="FirebaseAuthProvider"/> </br>
+        /// <br> DefaultRequestVersion = <see cref="HttpVersion.Version20"/> </br>
+        /// </summary>
+        public static IHttpClientBuilder AddFirebaseHttpClient(this IServiceCollection services) =>
+            services.AddHttpClient(nameof(FirebaseAuthProvider), x => x.DefaultRequestVersion = HttpVersion.Version20);
+    }
+}
